d6388dcf5c2f112062a0cbf00763e03d4a7387b1
dattebayoRob <robson.william65@gmail.com>
2019-04-12 21:00:43 -0300

    Initial Commit

---
+++ src/main/java/com/dtb/springdatabase/SpringDatabaseTestsApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.springdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtb/springdatabase/SpringDatabaseTestsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dtb.springdatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDatabaseTestsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
